19e3b283b40b7e29764a4530dbec2986b505bf7e
Christoph Honal <christoph.honal@web.de>
2026-05-16 06:54:05 +0200

    Use proper bodybytes board definition

---
+++ include/configs/bodybytes.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __CONFIG_BODYBYTES_H
+#define __CONFIG_BODYBYTES_H
+
+#include <configs/mt7628.h>
+
+/* UART2 (console) MMIO base — palmbus base 0xb0000000 + offset 0xe00 */
+#if defined(CONFIG_XPL_BUILD) && defined(CONFIG_SPL_SERIAL)
+#define CFG_SYS_NS16550_COM3		0xb0000e00
+#endif
+
+#endif /* __CONFIG_BODYBYTES_H */
